231874a8571682c90e5e747d2259c5cfda54ec6f
abdulkader-safi <safi.abdulkader@gmail.com>
2022-12-11 21:03:48 +0200

    ..

--- src/Components/ContentBody/Experience.tsx
+++ src/Components/ContentBody/Experience.tsx
@@ -17,7 +17,7 @@ const Experience = () => {
           <div className="flex-grow-1">
             <h3 className="mb-0">Full stack Web Developer</h3>
             <div className="subheading mb-3">Internship</div>
-            <p>Internship in Verizon as full stack web developer using reactJS in front end and laravel back end development connecting front and back end using APIs</p>
+            <p>Internship in Verazone as full stack web developer using reactJS in front end and laravel back end development connecting front and back end using APIs</p>
           </div>
           <div className="flex-shrink-0"><span className="text-primary">Fall 2022 - Present</span></div>
         </div>
